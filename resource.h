a5b621e060458be19848b0bfe4a409aad7140fc6
Yibang Heng <yibang.heng@outlook.com>
2023-06-23 11:43:57 +0800

    Add icons

---
+++ resource.h
@@ -0,0 +1,125 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by xicons.rc
+//
+#define IDI_ICON1                       101
+#define IDI_ICON2                       102
+#define IDI_ICON3                       103
+#define IDI_ICON4                       104
+#define IDI_ICON5                       105
+#define IDI_ICON6                       106
+#define IDI_ICON7                       107
+#define IDI_ICON8                       108
+#define IDI_ICON9                       109
+#define IDI_ICON10                      110
+#define IDI_ICON11                      111
+#define IDI_ICON12                      112
+#define IDI_ICON13                      113
+#define IDI_ICON14                      114
+#define IDI_ICON15                      115
+#define IDI_ICON16                      116
+#define IDI_ICON17                      117
+#define IDI_ICON18                      118
+#define IDI_ICON19                      119
+#define IDI_ICON20                      120
+#define IDI_ICON21                      121
+#define IDI_ICON22                      122
+#define IDI_ICON23                      123
+#define IDI_ICON24                      124
+#define IDI_ICON25                      125
+#define IDI_ICON26                      126
+#define IDI_ICON27                      127
+#define IDI_ICON28                      128
+#define IDI_ICON29                      129
+#define IDI_ICON30                      130
+#define IDI_ICON31                      131
+#define IDI_ICON32                      132
+#define IDI_ICON33                      133
+#define IDI_ICON34                      134
+#define IDI_ICON35                      135
+#define IDI_ICON36                      136
+#define IDI_ICON37                      137
+#define IDI_ICON38                      138
+#define IDI_ICON39                      139
+#define IDI_ICON40                      140
+#define IDI_ICON41                      141
+#define IDI_ICON42                      142
+#define IDI_ICON43                      143
+#define IDI_ICON44                      144
+#define IDI_ICON45                      145
+#define IDI_ICON46                      146
+#define IDI_ICON47                      147
+#define IDI_ICON48                      148
+#define IDI_ICON49                      149
+#define IDI_ICON50                      150
+#define IDI_ICON51                      151
+#define IDI_ICON52                      152
+#define IDI_ICON53                      153
+#define IDI_ICON54                      154
+#define IDI_ICON55                      155
+#define IDI_ICON56                      156
+#define IDI_ICON57                      157
+#define IDI_ICON58                      158
+#define IDI_ICON59                      159
+#define IDI_ICON60                      160
+#define IDI_ICON61                      161
+#define IDI_ICON62                      162
+#define IDI_ICON63                      163
+#define IDI_ICON64                      164
+#define IDI_ICON65                      165
+#define IDI_ICON66                      166
+#define IDI_ICON67                      167
+#define IDI_ICON68                      168
+#define IDI_ICON69                      169
+#define IDI_ICON70                      170
+#define IDI_ICON71                      171
+#define IDI_ICON72                      172
+#define IDI_ICON73                      173
+#define IDI_ICON74                      174
+#define IDI_ICON75                      175
+#define IDI_ICON76                      176
+#define IDI_ICON77                      177
+#define IDI_ICON78                      178
+#define IDI_ICON79                      179
+#define IDI_ICON80                      180
+#define IDI_ICON81                      181
+#define IDI_ICON82                      182
+#define IDI_ICON83                      183
+#define IDI_ICON84                      184
+#define IDI_ICON85                      185
+#define IDI_ICON86                      186
+#define IDI_ICON87                      187
+#define IDI_ICON88                      188
+#define IDI_ICON89                      189
+#define IDI_ICON90                      190
+#define IDI_ICON91                      191
+#define IDI_ICON92                      192
+#define IDI_ICON93                      193
+#define IDI_ICON94                      194
+#define IDI_ICON95                      195
+#define IDI_ICON96                      196
+#define IDI_ICON97                      197
+#define IDI_ICON98                      198
+#define IDI_ICON99                      199
+#define IDI_ICON100                     200
+#define IDI_ICON101                     201
+#define IDI_ICON102                     202
+#define IDI_ICON103                     203
+#define IDI_ICON104                     204
+#define IDI_ICON105                     205
+#define IDI_ICON106                     206
+#define IDI_ICON107                     207
+#define IDI_ICON108                     208
+#define IDI_ICON109                     209
+#define IDI_ICON110                     210
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        211
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
